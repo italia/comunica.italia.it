84fd6ca890bbdaddbc2674280d54b0587ef3df22
giorgio.duminuco <giorgio.duminuco@sixeleven.it>
2018-07-06 10:56:16 +0200

    Added scroll down linee guida front page

--- html/web/themes/custom/comunicaitalia/src/js/scrolling.js
+++ html/web/themes/custom/comunicaitalia/src/js/scrolling.js
@@ -2,8 +2,12 @@
   Drupal.behaviors.comunicaitalia_scrolling = {
     attach: function (context, settings) {
 
+      $('.scroll-down-wrapper').click(function() {
+        $("html, body").animate({scrollTop: $('#block-six8theme-views-block-linee-guida-list-block-1 h2').offset().top}, 450, 'swing');
+      });
+
       $('.scroll-top-wrapper').click(function() {
-        $("html, body").animate({scrollTop:0}, 600, 'swing');
+        $("html, body").animate({scrollTop: 0}, 600, 'swing');
       });
 
     }
