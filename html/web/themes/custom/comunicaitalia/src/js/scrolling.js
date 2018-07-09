c742b252b12eca8b5cedd87e7188295750f24f13
giorgio.duminuco <giorgio.duminuco@sixeleven.it>
2018-07-09 13:18:15 +0200

    Exported configuration

--- html/web/themes/custom/comunicaitalia/src/js/scrolling.js
+++ html/web/themes/custom/comunicaitalia/src/js/scrolling.js
@@ -2,14 +2,32 @@
   Drupal.behaviors.comunicaitalia_scrolling = {
     attach: function (context, settings) {
 
+      /* SCROLL DOWN FOR FRONTPAGE */
       $('.scroll-down-wrapper').click(function() {
         $("html, body").animate({scrollTop: $('#block-six8theme-views-block-linee-guida-list-block-1 h2').offset().top}, 450, 'swing');
       });
 
+      /* SCROLL DOWN FOR BASIC PAGES - GENERAL */
       $('.scroll-down-basic-wrapper').click(function() {
         $("html, body").animate({scrollTop: $('.main-wrapper').offset().top}, 450, 'swing');
       });
 
+      /* SCROLL DOWN FOR GUIDELINES PAGE */
+      if($('body').hasClass('page-node-21')) {
+        $('.scroll-down-basic-wrapper').click(function() {
+          $("html, body").animate({scrollTop: $('#block-six8theme-views-block-linee-guida-list-block-2').offset().top}, 450, 'swing');
+        });
+      }
+
+      /* SCROLL DOWN FOR ROADMAP PAGE */
+      if($('body').hasClass('path-roadmap')) {
+        // $('.scroll-down-basic-wrapper').click(function() {
+          console.log($('#block-six8theme-content'));
+        //   $("html, body").animate({scrollTop: $('#block-six8theme-content').offset().top}, 450, 'swing');
+        // });
+      }
+
+      /* SCROLL TOP FOR FOOTER */
       $('.scroll-top-wrapper').click(function() {
         $("html, body").animate({scrollTop: 0}, 600, 'swing');
       });
