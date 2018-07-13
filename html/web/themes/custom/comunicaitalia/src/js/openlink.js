325e952876419368aaec354039700d37a635d7bb
giorgio.duminuco <giorgio.duminuco@sixeleven.it>
2018-07-13 15:14:36 +0200

    External links for contatti page

---
+++ html/web/themes/custom/comunicaitalia/src/js/openlink.js
@@ -0,0 +1,37 @@
+(function ($) {
+  Drupal.behaviors.comunicaitalia_openlink = {
+    attach: function (context, settings) {
+
+
+      if($('body').hasClass('path-roadmap')) {
+        /* SCROLL DOWN FOR ROADMAP PAGE */
+
+      } else if($('body').hasClass('page-node-type-kit')) {
+        /* SCROLL DOWN FOR SINGLE KIT PAGE */
+
+      } else if($('body').hasClass('page-node-21')) {
+        /* SCROLL DOWN FOR GUIDELINES PAGE */
+
+      } else if($('body').hasClass('page-node-26')) {
+        /* TARGET BLANK FOR CONTATTI PAGE */
+        openBlank('.field-name-field_link .field__item a');
+        openBlank('.field-name-field_social .field__item a');
+        openBlank('.field-name-field_link_agid a');
+        openBlank('.field-name-field_call_to_action_pagina a');
+
+      } else {
+        /* SCROLL DOWN FOR BASIC PAGES - GENERAL */
+
+      }
+
+      function openBlank(selector) {
+        $(selector).click(function(e) {
+          e.preventDefault();
+          var url = $(this).attr('href');
+          window.open(url, '_blank');
+        });
+      }
+
+    }
+  };
+})(jQuery);
