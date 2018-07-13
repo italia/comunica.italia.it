53e4cd840fccd7b9ad745eb67890689f0086d045
giorgio.duminuco <giorgio.duminuco@sixeleven.it>
2018-07-13 15:04:25 +0200

    Fixed scrolling icons

--- html/web/themes/custom/comunicaitalia/src/js/scrolling.js
+++ html/web/themes/custom/comunicaitalia/src/js/scrolling.js
@@ -13,10 +13,23 @@
           console.log($('#block-six8theme-content').offset().top);
           $("html, body").animate({scrollTop: $('footer').offset().top}, 450, 'swing');
         });
+      } else if($('body').hasClass('page-node-type-kit')) {
+        /* SCROLL DOWN FOR SINGLE KIT PAGE */
+        $('.scroll-down-basic-wrapper').click(function() {
+          $("html, body").animate({scrollTop: $('.field-name-body').offset().top}, 450, 'swing');
+        });
+        $('.scroll-down-kit-wrapper').click(function() {
+          $("html, body").animate({scrollTop: $('.field-name-field_kit_files').offset().top}, 450, 'swing');
+        });
       } else if($('body').hasClass('page-node-21')) {
         /* SCROLL DOWN FOR GUIDELINES PAGE */
         $('.scroll-down-basic-wrapper').click(function() {
           $("html, body").animate({scrollTop: $('#block-six8theme-views-block-linee-guida-list-block-2').offset().top}, 450, 'swing');
+        });
+      } else if($('body').hasClass('page-node-26')) {
+        /* SCROLL DOWN FOR CONTATTI PAGE */
+        $('.scroll-down-basic-wrapper').click(function() {
+          $("html, body").animate({scrollTop: $('.field-name-body').offset().top}, 450, 'swing');
         });
       } else {
         /* SCROLL DOWN FOR BASIC PAGES - GENERAL */
